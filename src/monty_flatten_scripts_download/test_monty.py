e974587cd6f98db3b9973d675906f67e4fdcee20
ediakatos <ediakatos@mapaction.org>
2024-11-27 12:34:05 +0000

    json checker

---
+++ src/monty_flatten_scripts_download/test_monty.py
@@ -0,0 +1,31 @@
+import os
+import requests
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+base_url = "https://monty-api.ifrc.org/data/JSON"
+
+api_token = os.getenv("MONTY_API_TOKEN")
+
+params = {
+    "Mtoken": api_token,
+    "sdate": "2024-11-27",
+}
+
+response = requests.get(base_url, params=params)
+
+if response.status_code == 200:
+    data = response.json()
+    
+    impact_data_0 = data.get("impact_Data", [])[0]
+    
+    if impact_data_0:
+        with open("impact_data_0.json", "w") as json_file:
+            json.dump(impact_data_0, json_file, indent=4)
+        print("impact_Data[0] saved to 'impact_data_0.json'.")
+    else:
+        print("No impact_Data[0] found in the response.")
+else:
+    print(f"Failed to fetch data. HTTP Status Code: {response.status_code}")
