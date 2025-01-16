6cb177e5e88bac7929373fc706a859f3e3765d28
ediakatos <ediakatos@mapaction.org>
2025-01-16 12:35:23 +0000

    First save exploration of the dataset

--- src/idmc/data_normalisation_idmc.py
+++ src/idmc/data_normalisation_idmc.py
@@ -19,5 +19,10 @@ with open(SCHEMA_PATH_IDMC, "r") as schema_idmc:
     idmc_schema = json.load(schema_idmc)
 
 idmc_df_raw = pd.read_csv(IDMC_INPUT_CSV)
-print("Preview of raw data:")
-print(idmc_df_raw.head())
+
+# print("Preview of raw data:")
+# print(idmc_df_raw.head())
+
+cleaned1_df = map_and_drop_columns(idmc_df_raw, IDMC_MAPPING)
+os.makedirs("./data_mid/idmc_idu", exist_ok=True)
+cleaned1_df.to_csv("./data_mid/idmc_idu/idus_all_cleaned1.csv", index=False)
