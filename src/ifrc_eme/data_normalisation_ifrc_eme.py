34f6f55d6a363ab2c0f14681cb3ce3dcb55fc68a
ediakatos <ediakatos@mapaction.org>
2025-01-17 10:48:27 +0000

    Updated the script and fixed dates

--- src/ifrc_eme/data_normalisation_ifrc_eme.py
+++ src/ifrc_eme/data_normalisation_ifrc_eme.py
@@ -18,11 +18,13 @@ SCHEMA_PATH_IFRC_EME = "./src/ifrc_eme/ifrc_eme_schema.json"
 with open(SCHEMA_PATH_IFRC_EME, "r") as schema_ifrc_eme:
     ifrc_eme_schema = json.load(schema_ifrc_eme)
 
-ifrc_eme_df_raw = pd.read_csv(IFRC_EME_INPUT_CSV)
+def clean_disaster_start_date(df: pd.DataFrame, column_name: str) -> pd.DataFrame:
+    if column_name in df.columns:
+        df[column_name] = df[column_name].str.split('T').str[0]  # Extract part before 'T'
+    return df
 
-# print("Preview of raw data:")
-# print(ifrc_eme_df_raw.head())
-
+ifrc_eme_df_raw = pd.read_csv(IFRC_EME_INPUT_CSV, on_bad_lines="skip")
+ifrc_eme_df_raw = clean_disaster_start_date(ifrc_eme_df_raw, 'disaster_start_date')
 cleaned1_df = map_and_drop_columns(ifrc_eme_df_raw, IFRC_EME_MAPPING)
 # print("Preview of cleaned data:")
 # print(cleaned1_df.head())
