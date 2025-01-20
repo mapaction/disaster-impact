41b6a2373dd4ef2ec15bcf228fc6d6fd35c56593
ediakatos <ediakatos@mapaction.org>
2025-01-20 12:57:19 +0000

    Creating the directory

--- src/data_consolidation/data_consolidation.py
+++ src/data_consolidation/data_consolidation.py
@@ -4,6 +4,8 @@ import os
 
 from src.data_consolidation.dictionary import STANDARD_COLUMNS
 
+os.makedirs('./data_mid/data_standardised', exist_ok=True)
+
 UNIFIED_SCHEMA_PATH = "./src/unified/json_schemas/unified_schema.json"
 
 dataframes = {
@@ -20,3 +22,34 @@ with open(UNIFIED_SCHEMA_PATH, 'r') as f:
     schema = json.load(f)
 
 schema_properties = schema["properties"]
+
+def get_default_value(data_type):
+    if data_type == "string":
+        return None
+    elif data_type == "array":
+        return []
+    elif data_type == "integer":
+        return None
+    elif data_type == "number":
+        return None
+    else:
+        return None
+    
+def ensure_columns(df, standard_columns, schema_properties):
+    for column in standard_columns:
+        if column not in df.columns:
+            data_type = schema_properties.get(column, {}).get("type", ["string"])[0]
+            default_value = get_default_value(data_type)
+            
+            df[column] = [default_value] * len(df)
+    
+    df = df[standard_columns]
+    return df
+
+standardised_dataframes = {}
+for name, df in dataframes.items():
+    standardised_dataframes[name] = ensure_columns(df, STANDARD_COLUMNS, schema_properties)
+
+for name, df in standardised_dataframes.items():
+    output_path = f'./data_mid/data_standardised/{name}_standardised.csv'
+    df.to_csv(output_path, index=False)
