7c3e40ab177267132d5e86d39334fe905f97770c
ediakatos <ediakatos@mapaction.org>
2025-01-24 09:37:13 +0000

    Restructuring dc scripts

--- src/gdacs/combine_csv_gdacs.py
+++ src/gdacs/combine_csv_gdacs.py
@@ -3,7 +3,7 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from adam.combine_csv import combine_csvs
+from utils.combine_csv import combine_csvs
 
 input_dir = "/home/evangelos/src/disaster-impact/data/gdacs_all_types_yearly_v3_fast"
 output_file = "/home/evangelos/src/disaster-impact/data/gdacs_all_types_yearly_v3_fast/combined_gdacs_data.csv"
