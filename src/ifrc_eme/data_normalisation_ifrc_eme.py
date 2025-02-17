9e2a97cad6bb13fa0a7248d758f40c5e106d824f
ediakatos <ediakatos@mapaction.org>
2025-02-17 11:44:58 +0000

    read the idmc ec as well

--- src/ifrc_eme/data_normalisation_ifrc_eme.py
+++ src/ifrc_eme/data_normalisation_ifrc_eme.py
@@ -2,12 +2,13 @@ import pandas as pd
 import json
 import os
 
-from src.glide.data_normalisation_glide import map_and_drop_columns, change_data_type
+from src.glide.data_normalisation_glide import map_and_drop_columns, change_data_type, normalize_event_type
 from src.data_consolidation.dictionary import IFRC_EME_MAPPING
 from src.utils.azure_blob_utils import read_blob_to_dataframe
 
 IFRC_EME_INPUT_BLOB = "disaster-impact/raw/ifrc_dref/IFRC_emergencies.csv"
 SCHEMA_PATH_IFRC_EME = "./src/ifrc_eme/ifrc_eme_schema.json"
+EVENT_CODE_CSV = "./static_data/event_code_table.csv"
 
 with open(SCHEMA_PATH_IFRC_EME, "r") as schema_ifrc_eme:
     ifrc_eme_schema = json.load(schema_ifrc_eme)
@@ -39,6 +40,15 @@ def main():
     cleaned1_df = map_and_drop_columns(ifrc_eme_df_raw, IFRC_EME_MAPPING)
     cleaned2_df = change_data_type(cleaned1_df, ifrc_eme_schema)
     cleaned2_df['Date'] = pd.to_datetime(cleaned2_df['Date'], errors='coerce')
+    cleaned2_df = normalize_event_type(cleaned2_df, EVENT_CODE_CSV)
+
+    # Reorder of schema columns
+    schema_order = list(ifrc_eme_schema["properties"].keys())
+    ordered_columns = [col for col in schema_order if col in cleaned2_df.columns]
+    remaining_columns = [col for col in cleaned2_df.columns if col not in schema_order]
+    final_columns_order = ordered_columns + remaining_columns
+    cleaned2_df = cleaned2_df[final_columns_order]
+    #
 
     os.makedirs("./data_mid_1/ifrc_eme", exist_ok=True)
     output_file_path = "./data_mid_1/ifrc_eme/ifrc_eme_mid1.csv"
