0c71cde921ede7278ebf44199b7d24cb4b11aab2
ediakatos <ediakatos@mapaction.org>
2025-02-24 13:18:10 +0000

    Updateing combine_csv

--- src/gdacs/combine_csv_gdacs.py
+++ src/gdacs/combine_csv_gdacs.py
@@ -1,10 +1,15 @@
-import sys
-import os
+"""Combine all the csv files in the input directory into a single csv file."""
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent.resolve()))
 
 from utils.combine_csv import combine_csvs
 
 input_dir = "/home/evangelos/src/disaster-impact/data/gdacs_all_types_yearly_v3_fast"
-output_file = "/home/evangelos/src/disaster-impact/data/gdacs_all_types_yearly_v3_fast/combined_gdacs_data.csv"
+output_file = (
+    "/home/evangelos/src/disaster-impact"
+    "/data/gdacs_all_types_yearly_v3_fast/combined_gdacs_data.csv"
+)
 combine_csvs(input_dir, output_file)
