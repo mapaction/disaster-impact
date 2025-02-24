01ac15f7abd21da88ae2d7bd6f6b8007988ff341
ediakatos <ediakatos@mapaction.org>
2025-02-24 12:18:20 +0000

    Updating the upload to blob from unified

--- src/unified/upload_to_blob.py
+++ src/unified/upload_to_blob.py
@@ -1,5 +1,7 @@
+"""Script for uploading 'data_mid' to their corresponding Azure Blob."""
+
 from src.utils.azure_blob_utils import upload_dir_to_blob
 
 if __name__ == "__main__":
-    upload_dir_to_blob("data_mid", "mid")
+    upload_dir_to_blob("data_prep", "prep")
     upload_dir_to_blob("data_out", "out")
